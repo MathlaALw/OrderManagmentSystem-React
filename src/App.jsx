8ad86188d19bdb29713100851f437a344f746a2b
Mathla Alwahaibi <mathlaalwahaibi95@gmail.com>
2025-12-14 00:11:24 +0400

    Refactor bootstrap imports

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@
 import './App.css'
 import Login from './components/Main/Login/Login'
 
+// import bootstrap css and js
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
 
 function App() {
 
